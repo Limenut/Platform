8c9f1c020d0bed6de0b38cf7e06e544bacf67748
Limenut <olli.myllymaki@student.lamk.fi>
2016-05-13 10:51:18 +0300

    trying

--- Platform/main.cpp
+++ Platform/main.cpp
@@ -10,12 +10,24 @@
 using namespace std;
 using namespace std::chrono;
 
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 800;
+Window mainWindow;
+
 struct doubleVector
 {
 	double x;
 	double y;
 };
 
+enum Direction
+{
+	NONE,
+	LEFT,
+	RIGHT,
+	DOWN,
+	UP
+};
 
 class Character
 {
@@ -28,49 +40,71 @@ public:
 	double runSpeed;
 	double airSpeed;
 	double jumpVelocity;
+	double jumpHeight;
+	double jumpHeightMax;
 	doubleVector position;
 	doubleVector origin;
 
 	SDL_Rect rect;
 
 	bool airBorne;
+	bool freeFall;
+	Direction primDirection;
 };
 
 Character::Character()
 {
-	velocity.x		= 0.0;
-	velocity.y		= 0.0;
-	gravity			= 0.0;
-	runSpeed		= 0.0;
-	jumpVelocity	= 0.0;
-	position.x		= 0.0;
-	position.y		= 0.0;
-	origin.x		= 0.0;
-	origin.y		= 0.0;
-	rect.x			= 0;
-	rect.y			= 0;
-	rect.w			= 0;
-	rect.h			= 0;
-	airBorne		= false;
+	velocity.x = 0.0;
+	velocity.y = 0.0;
+	gravity = 0.0;
+	runSpeed = 0.0;
+	jumpVelocity = 0.0;
+	jumpHeight = 0.0;
+	jumpHeightMax = 0.0;
+	position.x = 0.0;
+	position.y = 0.0;
+	origin.x = 0.0;
+	origin.y = 0.0;
+	rect.x = 0;
+	rect.y = 0;
+	rect.w = 0;
+	rect.h = 0;
+	airBorne = false;
+	freeFall = false;
+	primDirection = NONE;
 }
 
 void Character::move(double deltaTime)
 {
 	if (airBorne)
 	{
-		velocity.y += gravity * deltaTime;
 		position.y += velocity.y * deltaTime;
+		if (freeFall)
+		{
+			velocity.y -= gravity * deltaTime;
+		}
+		else
+		{
+			jumpHeight += velocity.y * deltaTime;
+			if (jumpHeight >= jumpHeightMax)
+			{
+				position.y -= jumpHeight - jumpHeightMax;
+
+				jumpHeight = 0.0;
+				freeFall = true;
+			}
+		}
 	}
+
+
 
 	position.x += velocity.x * deltaTime;
 
 	rect.x = int(position.x - origin.x + 0.5);
-	rect.y = int(position.y - origin.y + 0.5);
+	rect.y = SCREEN_HEIGHT - int(position.y - origin.y + 0.5);
 }
 
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 800;
-Window mainWindow;
+
 
 bool init()
 {
@@ -104,15 +138,16 @@ int main()
 
 	Character Player;
 	Player.position.x = 100;
-	Player.position.y = 800;
-	Player.gravity = 3000.0;
+	Player.position.y = 0;
+	Player.gravity = 5000.0;
 	Player.runSpeed = 500.0;
 	Player.jumpVelocity = 1200.0;
+	Player.jumpHeightMax = 200;
 
 	Player.rect.w = 32;
 	Player.rect.h = 64;
 	Player.origin.x = (double)(Player.rect.w / 2);
-	Player.origin.y = (double)Player.rect.h;
+	Player.origin.y = -(double)Player.rect.h;
 
 	Player.rect.x = int(Player.position.x + 0.5);
 	Player.rect.y = int(Player.position.y + 0.5);
@@ -124,30 +159,38 @@ int main()
 	while (true)
 	{
 		frameTime = duration_cast<microseconds>(system_clock::now() - lastTime).count() / 1000000.0;
+		if (frameTime > 0.1) frameTime = 0.1;	//at low framerates game becomes frame dependent to avoid collision errors etc.
 		lastTime = system_clock::now();
 
-		
+
 		SDL_PumpEvents();
 
 		if (keystate[SDL_SCANCODE_RIGHT] && !keystate[SDL_SCANCODE_LEFT]) Player.velocity.x = Player.runSpeed;
 		else if (keystate[SDL_SCANCODE_LEFT] && !keystate[SDL_SCANCODE_RIGHT]) Player.velocity.x = -Player.runSpeed;
-		else Player.velocity.x = 0.0;
+		else if (!keystate[SDL_SCANCODE_LEFT] && !keystate[SDL_SCANCODE_RIGHT]) Player.velocity.x = 0.0;
 
-		if (keystate[SDL_SCANCODE_UP] && !Player.airBorne) 
+
+
+		if (keystate[SDL_SCANCODE_UP] && !Player.freeFall)
 		{
-			Player.velocity.y = -Player.jumpVelocity;
+			Player.velocity.y = Player.jumpVelocity;
 			Player.airBorne = true;
+		}
+		else if (Player.airBorne)
+		{
+			Player.freeFall = true;
 		}
 
 		Player.move(frameTime);
 
-		if (Player.position.y >= 800.0)
+		if (Player.position.y <= 0.0)
 		{
 			Player.airBorne = false;
-			Player.position.y = 800.0;
+			Player.freeFall = false;
+			Player.position.y = 0.0;
 		}
 
-		
+
 
 		SDL_SetRenderDrawColor(mainWindow.ren, 0, 0, 0, 255);
 		SDL_RenderClear(mainWindow.ren);
@@ -156,8 +199,9 @@ int main()
 		SDL_RenderFillRect(mainWindow.ren, &Player.rect);
 		SDL_RenderPresent(mainWindow.ren);
 
-		SDL_Delay(1);
+		//SDL_Delay(1);
 	}
 
 	close();
 }
+//boo
